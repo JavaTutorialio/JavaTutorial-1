11105c3622ea86c483a7af74b7b7871af0d8f1c8
iozberk <iozberks@gmail.com>
2022-06-28 22:33:28 +0300

    first committ

--- JavaTutorial/src/MainClass.java
+++ JavaTutorial/src/MainClass.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test github");
+		Scanner input = new Scanner (System.in);
+		String username="edkucuk",pasword="123456";
+		
+		String kullanici_adi, sifre;
+		System.out.println("Kullanıcı adını giriniz:");
+		kullanici_adi=input.next();
+		System.out.println("Şifrenizi giriniz:");
+		sifre=input.next();
+		
+		if (username.equals(kullanici_adi) && pasword.equals(sifre))
+		{
+			System.out.println ("Sisteme başarıyla girildi!");
+		}
+		else 
+		{
+			System.out.println ("Kullanıcı adı veya şifre yanlış!\nKontrol ederek tekarar deneyiniz.");
+		}
 	}
 
 }
---
+++ JavaTutorial/src/Sifre.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ExampleClass {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner (System.in);
+	String username="edkucuk",pasword="123456";
+	
+	String kullanici_adi, sifre;
+	System.out.println("Kullanıcı adını giriniz:");
+	kullanici_adi=input.next();
+	System.out.println("Şifrenizi giriniz:");
+	sifre=input.next();
+	
+	if (username.equals(kullanici_adi) && pasword.equals(sifre))
+	{
+		System.out.println ("Sisteme başarıyla girildi!");
+	}
+	else 
+	{
+		System.out.println ("Kullanıcı adı veya şifre yanlış!\nKontrol ederek tekarar deneyiniz.");
+	}
+	}
+}

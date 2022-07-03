b77cf8ff88ccd8d852688cb4c793c8e6c9110a71
iozberk <iozberks@gmail.com>
2022-07-03 17:30:29 +0300

    EE commit

--- JavaTutorial/src/Sifre.java
+++ JavaTutorial/src/Sifre.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ExampleClass {
+public class Sifre {
 
 	public static void main(String[] args) {
 	Scanner input = new Scanner (System.in);
--- JavaTutorial/src/tEST.java
+++ JavaTutorial/src/tEST.java
@@ -1,1 +1,10 @@
-Test
+import java.util.Scanner;
+
+public class tEST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test github");
+	}
+
+}
